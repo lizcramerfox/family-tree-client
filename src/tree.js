4967221443fc5812918aa1fa0fcbdf987a3912c3
lizcramerfox <ecfox2008@gmail.com>
2021-02-12 17:06:16 -0500

    Adds basic tree page and gen0 data

---
+++ src/tree.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Tree extends Component {
+  render () {
+    return (
+      <div>   
+        <span>Last Name, </span>
+        <span>First Name</span>
+      </div>
+    )
+  }
+}
+
+export default Tree

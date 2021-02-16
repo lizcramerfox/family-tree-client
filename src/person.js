3231d90f50c44670b7631571cf6e868bbf8a9e82
lizcramerfox <ecfox2008@gmail.com>
2021-02-16 17:30:47 -0500

    Adds name at birth to Person

--- src/person.js
+++ src/person.js
@@ -3,11 +3,27 @@ import React, { Component } from 'react'
 class Person extends Component {
   render () {
     const { firstName, middleName, lastName, maidenName, prefName, sex, birthYear, birthMonth, birthDay, birthCity, birthState, birthCountry, deathYear, deathMonth, deathDay, deathCity, deathState, deathCountry, generation } = this.props.person
+    
+    let firstMiddle, last
 
+    if (middleName === '') {
+      firstMiddle = `${firstName}`
+    } else {
+      firstMiddle = `${firstName} ${middleName}`
+    }
+
+    if (maidenName === '') {
+      last = `${lastName}`
+    } else {
+      last = `${maidenName}`
+    }
+
+    const birthName = `${firstMiddle} ${last}`
 
     return (
       <div className="person-profile">
         <h2>{prefName} {lastName}</h2>
+        <h5>born: {birthName}</h5>
         <p>b. {birthMonth}/{birthDay}/{birthYear} - {birthCity}, {birthState}, {birthCountry}</p>
         <p>d. {birthMonth}/{birthDay}/{birthYear} - {deathCity}, {deathState}, {deathCountry}</p>
       </div>

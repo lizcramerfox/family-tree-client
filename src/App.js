ade8bb3b4d79e01ce76d3dab412dd76eaba51d93
lizcramerfox <ecfox2008@gmail.com>
2021-02-24 14:32:57 -0500

    Adds Home component and adds react-router routes to App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import PersonIndex from './PersonIndex';
+import Home from './Home';
 import './App.scss';
+import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+ 
 
 function App() {
-
   return (
+  <Router>
     <div className="App">
       <header className="App-header">
         <h1>Family Tree</h1>
       </header>
       <main className="App-body">
-        <PersonIndex />
+        <Route path="/" component={Home} />
       </main>
     </div>
+  </Router>
   )
-};
+}  
 
-export default App;
+
+export default App
---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './App.scss'
+
+class Home extends Component {
+  render() {
+    return(
+      <h3 className="home">Welcome to the Heatherington Clan family tree!</h3>
+    )
+  }
+}
+
+export default Home
